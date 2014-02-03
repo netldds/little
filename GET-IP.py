228435493e421f1e2c02b41ac23cc174c04cde05
netldds <netldds@163.com>
2014-02-04 07:04:52 +0800

    NEW function:GET

---
+++ GET-IP.py
@@ -0,0 +1,10 @@
+import urllib2,bs4
+url="http://www.baidu.com/s?ie=UTF-8&wd=ip"
+
+def GET():
+    html=urllib2.urlopen(url).read()
+    soup=bs4.BeautifulSoup(html)
+    IP=soup.find("div",{"id":"1"})["fk"]
+    return IP
+if __name__=="__main__":
+    print GET()
